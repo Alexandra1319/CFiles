e6554c8f971a3582956a99f69813f6ed5cb521a7
Alexandra1319 <alexandramourouzidou@gmail.com>
2023-12-08 22:55:13 +0200

    why not work >:(

--- .vscode/.vscode/AlexandraMourouzidou_ergasia9.c
+++ .vscode/.vscode/AlexandraMourouzidou_ergasia9.c
@@ -14,11 +14,14 @@ int n;
 void fillPin2D(int [][n],int );
 void showPin2D(int [][n],int );
 void findMeanLine(int [][n],int ,double []);
+void showPinDouble1D(double [],int );
+void findMeanCol(int [][n],int ,double []);
 
 int main(int argc, char *argv[])
 {
     //askhsh 1
     
+    srand(time(NULL));
     int n; //diavasma timhs n>5
     
     do{
@@ -32,18 +35,25 @@ int main(int argc, char *argv[])
     showPin2D(my2DArray,n); //emfanisi pinaka me sinartisi
     
     double avgLine[n];
-    findMeanLine(my2DArray,n,avgLine); //emfanisi max me sinartisi
-  
+    findMeanLine(my2DArray,n,avgLine); //ypologismos max kata grammi me sinartisi
+    
+    showPinDouble1D(avgLine,n); //emfanisi pinaka max kata grammi
+
+    double avgCol[n];
+    findMeanCol(my2DArray,n,avgCol); //ypologismos max kata stili me sinartisi
+
+    showPinDouble1D(avgCol,n); //emfanisi pinaka max kata stili
+
   system("PAUSE");	
   return 0;
 }
-          ////////functions//////////
+
+                 //////////functions//////////
           
 void fillPin2D(int my2DArray[][n],int n) //gemizw pinaka
 {  
     int i,j;
-    srand(time(NULL));
-    
+
     for (i=0;i<n;i++)
     {
         for (j=0;j<n;j++)
@@ -51,8 +61,7 @@ void fillPin2D(int my2DArray[][n],int n) //gemizw pinaka
     }
 }
 
-void showPin2D
-(int my2DArray[][n],int n) //emfanizw stoixia pinaka
+void showPin2D(int my2DArray[][n],int n) //emfanizw stoixia pinaka
 {
     int i,j;
      for (i=0;i<n;i++)
@@ -67,7 +76,7 @@ void showPin2D
     }
 }
 
-void findMeanLine(int my2DArray[][n],int n,double avgLine[])
+void findMeanLine(int my2DArray[][n],int n,double avgLine[]) // vriskw max grammhs
 {
     int i,j,sumi;
     
@@ -78,6 +87,31 @@ void findMeanLine(int my2DArray[][n],int n,double avgLine[])
             sumi=sumi+my2DArray[i][j];
         avgLine[i]=(double)sumi/n;
     }
-    
+}
 
+void showPinDouble1D(double avg[],int n) //emfanizw max greammhs/stilis
+{   
+    int i;
+    for (i=0;i<n;i++)
+    {
+        if (i==0)
+            printf("\navgLine = %.2lf ",avg[i]);
+        else if (i==n-1)
+            printf("%.2lf\n",avg[i]);
+        else
+            printf("%.2lf ",avg[i]);
+    }
+}
+
+void findMeanCol(int my2DArray[][n],int n,double  avgCol[]) //vriskw max stilis
+{
+    int i,j,sumj;
+
+    for (j=0;j<n;j++)
+    {  
+        sumj=0;
+        for (i=0;i<n;i++)
+            sumj=sumj+my2DArray[i][j];
+        avgCol[j]=(double)sumj/n;
+    }
 }
